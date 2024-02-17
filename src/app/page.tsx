41a5b3e6e2816eb6002b7922d1466b9d5994e60e
Mat_dweb <110640534+Matdweb@users.noreply.github.com>
2024-02-16 23:42:43 -0600

    feat: add 'addNewTour' button as a footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 'use client'
 import AmerikVentureHeader from "@/components/AmerikVentureHeader";
+import TourBox from "@/components/TourBox";
+import { Heading, Text } from "@chakra-ui/react";
 
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-5">
+    <main className="flex min-h-screen flex-col items-center justify-start p-5 pb-32">
       <AmerikVentureHeader />
+      <TourBox />
+      <TourBox />
+      <TourBox />
+      <footer className='bg-[#959595] fixed bottom-0 w-full h-28 flex justify-center items-center'>
+        <Text
+          fontSize='2xl'
+          className='outline outline-2 px-4 py-2 outline-[#5d5d5d] rounded-md active:scale-95 transition-all duration-75 cursor-pointer'
+        >
+          + Añadir gira
+        </Text>
+      </footer>
     </main >
   );
 }
