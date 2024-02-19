3efc9ddada301112c9d5c6da5b43fe53615be564
Mat_dweb <110640534+Matdweb@users.noreply.github.com>
2024-02-19 13:45:03 -0600

    feat(TourBox): add hover animation

--- src/components/TourBox.tsx
+++ src/components/TourBox.tsx
@@ -27,14 +27,14 @@ function TourBox() {
 
     return (
         <div
-            className='w-72 h-28 mt-12 flex justify-between items-center flex-row rounded-lg active:scale-95 transition-all duration-75 cursor-pointer'
+            className='min-w-64 w-full h-28 mt-12 flex justify-between items-center flex-row rounded-lg active:scale-95 transition-all duration-75 cursor-pointer select-none group'
             style={{ boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px' }}
         >
             <Heading as='h3' size='2xl' className='pl-4'>
                 Gira 1
             </Heading>
             <div
-                className='w-16 hover:w-12 h-full rounded-r-lg transition-all duration-700 ease-out'
+                className='w-16 h-full group-hover:w-12 rounded-r-lg transition-all duration-700 ease-out'
                 style={{ background: `rgb(${randomRGB.r}, ${randomRGB.g}, ${randomRGB.b})` }}
             ></div>
         </div>
