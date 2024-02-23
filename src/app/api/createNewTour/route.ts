408b2153a697e5d767debb81315598004dc8c215
Mat_dweb <110640534+Matdweb@users.noreply.github.com>
2024-02-22 23:50:29 -0600

    feat: create api/createNewTour endpoint

---
+++ src/app/api/createNewTour/route.ts
@@ -0,0 +1,28 @@
+import { connectMongoDB } from "@/lib/mongoDB/mongodb";
+import Tour from "@/models/tourSchema";
+
+export async function POST(request: Request) {
+
+    try {
+        const { newTour } = await request.json();
+
+        await connectMongoDB();
+        await Tour.create(newTour)
+
+        return Response.json(
+            {
+                status: 201,
+                statusText: 'Tour creation succesful'
+            }
+        );
+
+    } catch (e) {
+        console.log(e);
+        return Response.json(
+            {
+                status: 400,
+                statusText: 'There was an error creating the tour'
+            }
+        );
+    }
+}
