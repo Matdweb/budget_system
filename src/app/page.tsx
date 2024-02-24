2c6a66425e059ff41989445caffc048c6268cd8f
Mat_dweb <110640534+Matdweb@users.noreply.github.com>
2024-02-23 20:59:40 -0600

    feat: retrieve tours from data base and display them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,47 @@
 import AmerikVentureHeader from "@/components/AmerikVentureHeader";
 import TourBox from "@/components/TourBox";
 import AddNewTourFooter from "@/components/AddNewTourFooter";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { fetchTours } from "@/redux/features/toursSlice";
+import { Heading } from "@chakra-ui/react";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function Home() {
+  const dispatch = useAppDispatch();
+  const tours = useAppSelector(state => state.toursReducer.tours);
+  const isLoading = useAppSelector(state => state.toursReducer.isLoading);
+
+  useEffect(() => {
+    dispatch(fetchTours());
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-5 pb-32">
       <AmerikVentureHeader />
-      <TourBox />
-      <TourBox />
-      <TourBox />
+      {tours.map((tour) => {
+        return (
+          <TourBox
+            key={tour._id}
+            id={tour._id}
+            name={tour.name}
+            background={tour.background}
+          />
+        )
+      })}
+      {tours.length === 0 && !isLoading ?
+        <div className='w-full flex justify-center items-center pt-8 opacity-45'>
+          <Heading as='h3' size='lg'>
+            No hay giras creadas
+          </Heading>
+        </div>
+        :
+        ''
+      }
+
+      {isLoading &&
+        <LoadingSpinner />
+      }
       <AddNewTourFooter />
     </main >
   );
