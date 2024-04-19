42fe1dda118345209d635be291e805c195d743ff
Mat_dweb <110640534+Matdweb@users.noreply.github.com>
2024-04-19 00:08:49 -0600

    feat(api): create the tours/[tour_id]/addExpenses endpoint

---
+++ src/app/api/tours/[tour_id]/addExpenses/route.ts
@@ -0,0 +1,38 @@
+import { connectMongoDB } from "@/lib/mongoDB/mongodb";
+import Tour from "@/models/tourSchema";
+
+export async function POST(
+    request: Request,
+    { params }: { params: { tour_id: string } }
+) {
+
+    try {
+        const tour_id = params.tour_id;
+        const { expenses }: { expenses: number } = await request.json();
+
+        await connectMongoDB();
+        const tour = await Tour.findOne({ _id: tour_id });
+
+        tour.expenses.push(expenses);
+
+        await tour.save();
+
+        return Response.json(
+            {
+                status: 200,
+                statusText: 'Expenses added succesfully',
+                createdTour: tour
+            }
+        );
+
+    } catch (e) {
+        console.log(e);
+        return Response.json(
+            {
+                status: 400,
+                statusText: 'There was an error adding expenses to the tour',
+                createdTour: null
+            }
+        );
+    }
+}
